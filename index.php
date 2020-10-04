cdd127a20d15399ae51cc4f145c626b2ff3d1794
urii2 <urii2@LAPTOP-EHSFGVVM>
2020-10-04 21:06:45 +0200

    ol-exercici 5 funcio3

--- index.php
+++ index.php
@@ -18,6 +18,10 @@ and open the template in the editor.
         function ol_funcio2(){
             
         }
+        
+        function ol_funcio3(){
+            
+        }
 
 
         function OL_especial(){
